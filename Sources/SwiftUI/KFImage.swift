ec24c761109fdb48f7550be1cf05629528dc4157
martin.matejka <martinmatejka@me.com>
2020-11-18 14:55:28 +0100

    Fixed object access

--- Sources/SwiftUI/KFImage.swift
+++ Sources/SwiftUI/KFImage.swift
@@ -48,7 +48,7 @@ extension SwiftUI.Image {
 @available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
 public struct KFImage: SwiftUI.View {
     
-    struct ImageRequiredItems {
+    public struct ImageRequiredItems {
         let id: Int
         let forceInitialFetchImage: Bool
         let upadateImagePublisher: AnyPublisher<Void, Never>
